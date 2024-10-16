31935a72e7a3e4306db93953006801954b2a81a1
dmkret <dmkret@gmail.com>
2024-10-16 16:37:30 +0300

    add template functions

---
+++ src/lib/typing.ts
@@ -0,0 +1,7 @@
+import type { Action } from 'svelte/action';
+
+const typing: Action<HTMLElement, number> = (node, interval) => {
+	// Part of task 2
+};
+
+export default typing;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
+<script lang="ts">
+	import typing from '$lib/typing';
+
+	const apply = () => {
+		// Part of task 2
+	};
+
+	const reset = () => {
+		// Part of task 2
+	};
+</script>
+
 <main>
 	<input type="number" />
-	<button class="apply">Apply</button>
-	<button class="reset">Reset</button>
-	<div />
+	<button class="apply" on:click={apply}>Apply</button>
+	<button class="reset" on:click={reset}>Reset</button>
+	<div use:typing={100} />
 </main>
+
+<style>
+	/* Task 1 */
+</style>
